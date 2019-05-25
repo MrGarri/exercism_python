1a6c49670d54aed5580efbbb487c67766900ad2d
Roberto Garrido <garrihendrix@gmail.com>
2019-05-26 01:59:40 +0200

    Add protein translation

---
+++ protein-translation/protein_translation.py
@@ -0,0 +1,32 @@
+translations = {
+    'AUG': 'Methionine',
+    'UUU': 'Phenylalanine',
+    'UUC': 'Phenylalanine',
+    'UUA': 'Leucine',
+    'UUG': 'Leucine',
+    'UCU': 'Serine',
+    'UCC': 'Serine',
+    'UCA': 'Serine',
+    'UCG': 'Serine',
+    'UAU': 'Tyrosine',
+    'UAC': 'Tyrosine',
+    'UGU': 'Cysteine',
+    'UGC': 'Cysteine',
+    'UGG': 'Tryptophan',
+    'UAA': 'STOP',
+    'UAG': 'STOP',
+    'UGA': 'STOP'
+}
+
+
+def proteins(strand):
+    codons = [strand[i:i + 3] for i in range(0, len(strand), 3)]
+    result = []
+
+    for codon in codons:
+        protein = translations[codon]
+        if protein == 'STOP':
+            return result
+        result.append(protein)
+
+    return result
